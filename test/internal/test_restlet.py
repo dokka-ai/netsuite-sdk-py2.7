21dc6d3bc8eac4b9dfd07467d7d4916459cf5a3e
Dmitriy Golub <dmitriy.golub@gmail.com>
2021-04-19 12:48:32 +0300

    fix tests

--- test/internal/test_restlet.py
+++ test/internal/test_restlet.py
@@ -1,25 +1,25 @@
 
 def test_call_restlet(ns):
-    result = ns.call_get_restlet(9, 1, 'getVendorsCurrencies')
+    result = ns.call_get_restlet("https://6758546.restlets.api.netsuite.com/app/site/hosting/restlet.nl?script=9&deploy=1", 'getVendorsCurrencies')
     assert isinstance(result, dict)
     assert result['integrationType'] == 'getVendorsCurrencies'
     assert isinstance(result['data'], dict)
 
 
 def test_call_restlet_wrong_script(ns):
-    result = ns.call_get_restlet(10, 1, 'getVendorsCurrencies')
+    result = ns.call_get_restlet("https://6758546.restlets.api.netsuite.com/app/site/hosting/restlet.nl?script=10&deploy=1", 'getVendorsCurrencies')
     assert isinstance(result, dict)
     assert 'error' in result
 
 
 def test_call_restlet_wrong_deploy(ns):
-    result = ns.call_get_restlet(9, 2, 'getVendorsCurrencies')
+    result = ns.call_get_restlet("https://6758546.restlets.api.netsuite.com/app/site/hosting/restlet.nl?script=9&deploy=2", 'getVendorsCurrencies')
     assert isinstance(result, dict)
     assert 'error' in result
 
 
 def test_call_restlet_wrong_integration_type(ns):
-    result = ns.call_get_restlet(9, 1, 'getVendorsCurrencies1')
+    result = ns.call_get_restlet("https://6758546.restlets.api.netsuite.com/app/site/hosting/restlet.nl?script=9&deploy=1", 'getVendorsCurrencies1')
     assert isinstance(result, dict)
     assert result['status'] == 'failed'
     assert not isinstance(result['data'], dict)
