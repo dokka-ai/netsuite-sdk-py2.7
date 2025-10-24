a7d2190dcf88374e0d62f9f88e0890811ea39d48
Andrii Kozii <andrii.kozii@dokka.me>
2025-10-24 17:49:58 +0300

    DA-17189 Wrong exchange rate when doc date is today's date (#26)

--- netsuitesdk/api/currencyRate.py
+++ netsuitesdk/api/currencyRate.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 from .base import ApiBase
 import logging
+from datetime import datetime
 
 from ..internal.utils import PaginatedSearch
 
@@ -12,12 +13,37 @@ class CurrencyRate(ApiBase):
         ApiBase.__init__(self, ns_client=ns_client, type_name=u'CurrencyRate')
 
     def get_by_date(self, search_date, base_currency, transaction_currency):
+        # old deprecated version
         base_currency = self.ns_client.RecordRef(internalId=base_currency, type="currency")
         transaction_currency = self.ns_client.RecordRef(internalId=transaction_currency, type="currency")
 
         basic_search = self.ns_client.basic_search_factory(
             u'CurrencyRate',
             effectiveDate=self.ns_client.SearchDateField(searchValue=search_date, operator="onOrBefore"),
+            baseCurrency=self.ns_client.SearchMultiSelectField(searchValue=base_currency, operator="anyOf"),
+            transactionCurrency=self.ns_client.SearchMultiSelectField(searchValue=transaction_currency, operator="anyOf"),
+        )
+
+        paginated_search = PaginatedSearch(client=self.ns_client,
+                                           type_name=u'CurrencyRate',
+                                           # search_record=self.ns_client.CurrencyRateSearchBasic(),
+                                           basic_search=basic_search,
+                                           pageSize=20)
+        return list(self._paginated_search_to_generator(paginated_search=paginated_search))
+
+    def get_by_dates(self, from_date: datetime, to_date: datetime, base_currency, transaction_currency):
+        # new optimized version
+
+        base_currency = self.ns_client.RecordRef(internalId=base_currency, type="currency")
+        transaction_currency = self.ns_client.RecordRef(internalId=transaction_currency, type="currency")
+
+        basic_search = self.ns_client.basic_search_factory(
+            u'CurrencyRate',
+            effectiveDate=self.ns_client.SearchDateField(
+                operator="within",
+                searchValue=from_date,
+                searchValue2=to_date,
+            ),
             baseCurrency=self.ns_client.SearchMultiSelectField(searchValue=base_currency, operator="anyOf"),
             transactionCurrency=self.ns_client.SearchMultiSelectField(searchValue=transaction_currency, operator="anyOf"),
         )
